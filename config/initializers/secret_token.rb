43b0371fc0915172000e7457bae8ff6a4f98eddf
game7 <cmwoodall@game7.net>
2011-08-04 17:32:47 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firebird::Application.config.secret_token = '401449f0f7d5f48889e47115b6f4f64226f1250a397a05a2bd1c33a4b3cb1ca36d883f550babf7ed263c0cc0539c1b68cffa514d292f811e5f1db71eb56eda58'
