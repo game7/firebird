9b0820642b5032c27717fd077aaa3797db17a0c3
game7 <cmwoodall@game7.net>
2011-08-04 17:40:17 -0700

    first model

---
+++ app/models/referee_request_form.rb
@@ -0,0 +1,22 @@
+class RefereeRequestForm
+  include ActiveModel::Validations
+  include ActiveModel::Conversation
+  extend ActiveModel::Naming
+
+  attr_accessor :name, :email
+
+  validates_presence_of :name
+  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+
+
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
+
+  def persisted?
+    false
+  end
+
+end
