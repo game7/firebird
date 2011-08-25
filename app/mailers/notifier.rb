07f8cc8e0620b62158aa86b227f285a8dda1925c
game7 <cmwoodall@game7.net>
2011-08-24 22:48:03 -0700

    style changed; update emails

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,8 +3,8 @@ class Notifier < ActionMailer::Base
 
   def referee_request(request)
     @request = request
-    mail(:to => 'cmwoodall@game7.net', 
-          :cc => 'john.kosobud@dyha.org',
+    mail(:to => 'timeclass@aol.com', 
+          :cc => 'john.kosobud@dyha.org,jacque.gomez@dyha.org,cmwoodall@game7.net',
           :subject => 'DYHA Referee Request')
   end
 end
